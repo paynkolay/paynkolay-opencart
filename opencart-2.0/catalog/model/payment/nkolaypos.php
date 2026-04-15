0ea8c4c4225914f20b37d9a32c1f87ade4c357ab
osbm <osbm@osbm.dev>
2026-04-15 10:50:41 +0300

    opencart 2.0

--- opencart-2.0/catalog/model/payment/nkolaypos.php
+++ opencart-2.0/catalog/model/payment/nkolaypos.php
@@ -1,32 +1,36 @@
 <?php
-if (!defined("VERSION")) exit;
-class ModelPaymentNkolayPos extends Model {
-	public function getMethod($address, $total) {
-		$this->load->language('payment/nkolaypos');
+if (!defined('VERSION')) exit;
 
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('nkolaypos_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
+class ModelPaymentNkolayPos extends Model
+{
+    public function getMethod($address, $total)
+    {
+        $this->load->language('payment/nkolaypos');
 
-		if ($this->config->get('nkolaypos_total') > 0 && $this->config->get('nkolaypos_total') > $total) {
-			$status = false;
-		} elseif (!$this->config->get('nkolaypos_geo_zone_id')) {
-			$status = true;
-		} elseif ($query->num_rows) {
-			$status = true;
-		} else {
-			$status = false;
-		}
+        $query = $this->db->query(
+            "SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone
+             WHERE geo_zone_id = '" . (int)$this->config->get('nkolaypos_geo_zone_id') . "'
+             AND country_id = '" . (int)$address['country_id'] . "'
+             AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')"
+        );
 
-		$method_data = array();
+        if ($this->config->get('nkolaypos_total') > 0 && $this->config->get('nkolaypos_total') > $total) {
+            $status = false;
+        } elseif (!$this->config->get('nkolaypos_geo_zone_id')) {
+            $status = true;
+        } elseif ($query->num_rows) {
+            $status = true;
+        } else {
+            $status = false;
+        }
 
-		if ($status) {
-			$method_data = array(
-				'code'       => 'nkolaypos',
-				'title'      => $this->language->get('text_title'),
-				'terms'      => '',
-				'sort_order' => $this->config->get('nkolaypos_sort_order')
-			);
-		}
+        if (!$status) return [];
 
-		return $method_data;
-	}
+        return [
+            'code'       => 'nkolaypos',
+            'title'      => $this->language->get('text_title'),
+            'terms'      => '',
+            'sort_order' => $this->config->get('nkolaypos_sort_order'),
+        ];
+    }
 }
--- opencart-2.0/catalog/view/theme/default/template/payment/nkolaypos.tpl
+++ opencart-2.0/catalog/view/theme/default/template/payment/nkolaypos.tpl
@@ -1,10 +1,10 @@
-<form action="<?php echo $action ?>" method="post">
-  <?php foreach($data as $key => $value): ?>
-  <input type="hidden" name="<?php echo $key ?>" value="<?php echo $value ?>">
-  <?php endforeach ?>
-  <div class="buttons">
-    <div class="pull-right">
-      <input type="submit" value="<?php echo $button_confirm ?>" class="btn btn-primary" />
+<form action="<?php echo $action; ?>" method="post" id="paynkolay-form">
+    <?php foreach ($form_data as $key => $value): ?>
+    <input type="hidden" name="<?php echo htmlspecialchars($key); ?>" value="<?php echo htmlspecialchars($value); ?>">
+    <?php endforeach; ?>
+    <div class="buttons">
+        <div class="pull-right">
+            <input type="submit" value="<?php echo $button_confirm; ?>" class="btn btn-primary" />
+        </div>
     </div>
-  </div>
 </form>
